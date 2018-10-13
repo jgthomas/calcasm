cda886f7f2ca8af99f9728f19ec73be43ab7bbeb
James Thomas <codeactor@protonmail.com>
2018-10-13 12:01:24 +0100

    dedicated math functions file

---
+++ math.s
@@ -0,0 +1,54 @@
+.section .data
+
+.include "linux.s"
+.include "ascii.s"
+.include "logic.s"
+.include "numbers.s"
+
+
+.section .bss
+
+
+
+.section .text
+
+
+# FUNCTION: base_to_power
+#
+#    Calculate BASE**POWER.
+#
+# PARAMETERS
+#
+#    %rdi - BASE
+#    %rsi - POWER
+#
+# LOCAL VARIABLES
+#
+#    %rax - ongoing calculation of result
+#
+# RETURN
+#
+#    Integr result of BASE**POWER
+#
+.globl base_to_power
+.type base_to_power, @function
+base_to_power:
+        cmpq $0, %rsi
+        je to_power_zero
+
+        movq %rdi, %rax
+        dec %rsi
+
+loop_base_to_power:
+        cmpq $0, %rsi
+        je exit_base_to_power
+
+        mul %rdi                   # implicitly mul %rdi, %rax
+        dec %rsi
+        jmp loop_base_to_power
+
+to_power_zero:
+        movq $1, %rax
+
+exit_base_to_power:
+        ret
